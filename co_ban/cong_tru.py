f12065d0d272a050b6f890046acea403c75663b3
Phuong Le <lanphuongemily@gmail.com>
2026-05-23 11:45:29 +0700

    python basics

---
+++ co_ban/cong_tru.py
@@ -0,0 +1,10 @@
+a = int(input("nhap so a: "))
+b = int(input("nhap so b: "))
+cong = a + b
+print("a + b =", cong)
+tru = a - b
+print("a - b =", tru)
+nhan = a * b
+print("a * b =", nhan)
+chia = a / b
+print("a / b =", chia)
---
+++ co_ban/hinh_vuong.py
@@ -0,0 +1,6 @@
+chieuDai = float(input("nhap chieu dai: "))
+chieuRong = float(input("nhap chieu rong: "))
+chuVi = (chieuDai + chieuRong) * 2
+dienTich = chieuDai * chieuRong
+print("chu vi:", chuVi)
+print("dien tich:", dienTich)
