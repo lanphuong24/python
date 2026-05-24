93c6e4007894f56a87fd40af97712d4e9efd8f4d
luong <luongfox@gmail.com>
2026-05-24 08:52:43 +0700

    add ex

---
+++ cau_lenh_for/bt5.py
@@ -0,0 +1,4 @@
+# Tìm số 3 chữ số
+
+# Viết chương trình tìm tất cả số có 3 chữ số sao cho tổng tất cả các chữ số
+# bằng tích của chúng
--- co_ban/bt3.py
+++ co_ban/bt3.py
@@ -1,3 +1,5 @@
+## Giải phương trình bậc 1
+
 print("giai phuong trinh bac nhat ax + b = 0")
 a = float(input("nhap a: "))
 b = float(input("nhap b: "))
