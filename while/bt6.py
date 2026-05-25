fdc7c1ebe3fbc95b69b8063d5a44e3f1694a20f5
luong <luongfox@gmail.com>
2026-05-25 18:46:39 +0700

    Update folders name

---
+++ while/bt6.py
@@ -0,0 +1,10 @@
+# Trò chơi đoán số
+
+# Chương trình phát sinh số ngẫu nhiên từ 1 đến 10
+# Người chơi tham gia đoán đố đó, được đoán 3 lần.
+# Nếu người chơi đoán trúng, thông báo chiến thắng và dừng trò chơi.
+# Nếu sau 3 lần đoán thất bại, thông báo thua cuộc, hiển thị số bí mật, và dừng trò chơi.
+
+import random
+
+soBiMat = random.randint(1, 10)
