0df4bf411960562f5f65ed0285b505b222c44c7b
luong <luongfox@gmail.com>
2026-05-25 19:04:43 +0700

    Origanize layout

---
+++ for/bt3_1.py
@@ -0,0 +1,4 @@
+## Kiểm tra số nguyên tố
+
+# Nhập số nguyên n
+# Kiểm tra n có phải là số nguyên tố hay không.
---
+++ for/bt7_2.py
@@ -0,0 +1,4 @@
+## Tính tổng dãy số 1 + 1/2 + 1/3 + ... + 1/n
+
+# Nhập số nguyên n.
+# Tính tổng dãy số.
