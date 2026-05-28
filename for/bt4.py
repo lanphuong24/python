8ce3aa45e6772c00f8483c36e45bf3467414e17e
luong <luongfox@gmail.com>
2026-05-28 12:29:25 +0700

    Fix bt4 for

--- for/bt4.py
+++ for/bt4.py
@@ -3,3 +3,4 @@
 # Nhập số nguyên n
 # Kiểm tra n có phải là số hoàn hảo hay không. 
 # n được gọi là số hoàn hảo nếu tổng các ước số của nó (không bao gồm n) bằng chính nó.
+# VD: 6, 28 là số hoàn hảo.
